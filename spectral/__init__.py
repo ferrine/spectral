5c994f03cbbd4ede0592916c874ad3557ea21042
Maxim Kochurov <m.kochurov@partner.samsung.com>
2018-11-23 12:55:00 +0300

    remove tf import

--- spectral/__init__.py
+++ spectral/__init__.py
@@ -1,14 +1,3 @@
-try:
-    import tensorflow
-
-    del tensorflow
-except ImportError:
-    import warnings
-
-    warnings.warn(
-        "tensorflow load failed, you will probably fail to compute inception metrics"
-    )
-    del warnings
 from . import nets, generator, discriminator, utils, norm, datasets, gan, logging
 
 __version__ = "0.0.1"
